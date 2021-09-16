23269fc0967616397bc5f0bb9ad4a223939c0b0e
yeji4268 <yeji4268@naver.com>
2021-09-16 23:37:22 +0900

    v0.3

--- syntax04.go
+++ syntax04.go
@@ -1,26 +1,57 @@
-packge main
+package main
 
-import{
+import (
 	"fmt"
 	"math/rand"
 	"time"
-}
+)
 
-//난수 추출된 수의 소수 판정 프로그램 v0.2
+//난수 추출된 수의 소수 판정 프로그램 v0.3
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
-	count := 0
-	number := rand.Intn(150) + 2 //0과 1제외, 2~151 사이의 수
+	isPrime := true
+	number := rand.Intn(150) + 2
 	fmt.Println("임의로 추출된 수 : ", number)
 
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
-			count = count + 1
+			isPrime = false
+
 		}
 	}
+
+	if isPrime == true {
+		fmt.Println(number, "는(은) 소수입니다.")
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다. ")
+	}
 }
+
+// packge main
+
+// import{
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// }
+
+// //난수 추출된 수의 소수 판정 프로그램 v0.2
+// func main() {
+// 	seed := time.Now().Unix()
+// 	rand.Seed(seed)
+
+// 	count := 0
+// 	number := rand.Intn(150) + 2 //0과 1제외, 2~151 사이의 수
+// 	fmt.Println("임의로 추출된 수 : ", number)
+
+// 	for i := 2; i < number; i++ {
+// 		if number%i == 0 {
+// 			count = count + 1
+// 		}
+// 	}
+// }
 
 // package main
 
