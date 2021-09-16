5c4f392a15e16bc23f7a111a333f1acc465e03a4
yeji4268 <yeji4268@naver.com>
2021-09-16 23:42:15 +0900

    v0.4

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-//난수 추출된 수의 소수 판정 프로그램 v0.3
+//난수 추출된 수의 소수 판정 프로그램 v0.4
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -18,16 +18,47 @@ func main() {
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			isPrime = false
-
 		}
+		fmt.Print(i, " ")
 	}
 
-	if isPrime == true {
+	if isPrime {
 		fmt.Println(number, "는(은) 소수입니다.")
 	} else {
-		fmt.Println(number, "는(은) 소수가 아닙니다. ")
+		fmt.Println(number, "는(은) 소수가 아닙니다")
 	}
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
+
+// //난수 추출된 수의 소수 판정 프로그램 v0.3
+// func main() {
+// 	seed := time.Now().Unix()
+// 	rand.Seed(seed)
+
+// 	isPrime := true
+// 	number := rand.Intn(150) + 2
+// 	fmt.Println("임의로 추출된 수 : ", number)
+
+// 	for i := 2; i < number; i++ {
+// 		if number%i == 0 {
+// 			isPrime = false
+
+// 		}
+// 	}
+
+// 	if isPrime == true {
+// 		fmt.Println(number, "는(은) 소수입니다.")
+// 	} else {
+// 		fmt.Println(number, "는(은) 소수가 아닙니다. ")
+// 	}
+// }
 
 // packge main
 
